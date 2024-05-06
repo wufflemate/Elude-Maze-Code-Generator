47da50c7de8a130aff08a06c216f7dd4bd7e4efc
Wuffle <156601079+wufflemate@users.noreply.github.com>
2024-05-06 16:39:37 +0100

    Changed name of result2 script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calculate() {
     var number = parseInt(document.getElementById('numberInput').value);
     var result2 = number * 25 + 1093;
-    document.getElementById('result').innerHTML = "Result 2: " + result2;
+    document.getElementById('result').innerHTML = "The code to put in: " + result2;
 }
