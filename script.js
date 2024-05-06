980488be8260b2ce79ac16aaff8da93d09601324
Wuffle <156601079+wufflemate@users.noreply.github.com>
2024-05-06 20:03:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ document.addEventListener('mousemove', function(e) {
     var cursorBox = document.getElementById('cursorBox');
     cursorBox.style.left = e.pageX + 'px'; // Set left position of the box
     cursorBox.style.top = e.pageY + 'px'; // Set top position of the box
+
+    var cursorBox2 = document.getElementById('cursorBox2');
+    cursorBox2.style.left = e.pageX + 'px'; // Set left position of the box
+    cursorBox2.style.top = e.pageY + 'px'; // Set top position of the box
 });
