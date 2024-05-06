dde675e7fffe9da3c16e17920fb2969c8b38bb52
Wuffle <156601079+wufflemate@users.noreply.github.com>
2024-05-06 16:28:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function calculate() {
     var number = parseInt(document.getElementById('numberInput').value);
-    var result1 = number * 25 + 1100 - 7;
     var result2 = number * 25 + 1093;
-    document.getElementById('result').innerHTML = "Result 1: " + result1 + "<br>Result 2: " + result2;
+    document.getElementById('result').innerHTML = "Result 2: " + result2;
 }
